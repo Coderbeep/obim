53eca0a3a5d22c8201e4741e935f1a43a02c1270
coderbeep <jakubkubiak234@gmail.com>
2025-06-30 22:06:01 +0200

    DnD rework

--- src/renderer/src/hooks/useFileExplorerDragAndDrop.tsx
+++ src/renderer/src/hooks/useFileExplorerDragAndDrop.tsx
@@ -1,48 +1,87 @@
 import { moveFile, saveFile } from "@renderer/services/fileService";
 import {
   currentFilePathAtom,
+  dragCounterAtomFamily,
   editorNoteTextAtom,
   reloadFlagAtom,
 } from "../store/NotesStore";
-import { useAtom, useAtomValue } from "jotai";
-import { useState } from "react";
+import { dragContentAtom, dragRefAtom } from "../store/DragStore";
+import { useAtom, useStore } from "jotai";
 import { useFileOpen } from "./file-actions-hooks/useFileActions";
 import { FileItem } from "@shared/models";
 import { tryCatch, tryCatchSync } from "@shared/tryCatch";
 
-interface useFileExplorerDragAndDropProps {
-  targetDirectoryPath: string;
-}
+/**
+ * Hook for setting up drag source behavior on a file or directory.
+ * Interacts with GlobalDrag to prepare preview of the drag.
+ */
+export const useFileExplorerDragSource = (file: FileItem) => {
+  const store = useStore();
+  const dragImageRef = store.get(dragRefAtom);
+  const setDragContent = (content: string) =>
+    store.set(dragContentAtom, content);
 
-export const useFileExplorerDragAndDrop = ({
-  targetDirectoryPath,
-}: useFileExplorerDragAndDropProps) => {
-  const [dragCounter, setDragCounter] = useState(0);
-  const currentFilePath = useAtomValue(currentFilePathAtom);
-  const editorNoteText = useAtomValue(editorNoteTextAtom);
-  const [, setReloadFlag] = useAtom(reloadFlagAtom);
+  const onDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+    event.dataTransfer.setData("application/json", JSON.stringify(file));
+    setDragContent(file.filename);
+    event.dataTransfer.setDragImage(new Image(), 0, 0);
+    if (dragImageRef) {
+      dragImageRef.style.display = "flex";
+    }
+  };
+
+  const onDrag = (event: React.DragEvent<HTMLDivElement>) => {
+    if (dragImageRef) {
+      dragImageRef.style.position = "fixed";
+      dragImageRef.style.left = `${event.pageX + 10}px`;
+      dragImageRef.style.top = `${event.pageY + 5}px`;
+    }
+  };
+
+  const onDragEnd = () => {
+    if (dragImageRef) {
+      Object.assign(dragImageRef.style, {
+        display: "none",
+        left: `-200px`,
+        top: `-200px`,
+      });
+    }
+  };
+
+  return { onDragStart, onDrag, onDragEnd };
+};
+
+/**
+ * Hook for handling drop targets. Used by the FileExplorer
+ * and ListDirectory elements.
+ */
+
+export const useFileExplorerDropTarget = (targetDirectoryPath: string) => {
+  const store = useStore();
+  const [dragCounter, setDragCounter] = useAtom(
+    dragCounterAtomFamily(targetDirectoryPath)
+  );
+
+  const setReloadFlag = (flag: boolean) => store.set(reloadFlagAtom, flag);
   const { open } = useFileOpen();
 
-  const onDragEnter = (event: React.DragEvent<HTMLDivElement>): void => {
+  const onDragEnter = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
     event.stopPropagation();
     setDragCounter((prev) => prev + 1);
   };
 
-  const onDragOver = (event: React.DragEvent<HTMLDivElement>): void => {
+  const onDragOver = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
     event.stopPropagation();
   };
 
-  const onDragLeave = (event: React.DragEvent<HTMLDivElement>): void => {
+  const onDragLeave = (event: React.DragEvent<HTMLDivElement>) => {
     event.stopPropagation();
     setDragCounter((prev) => Math.max(0, prev - 1));
   };
 
-  // save the file and then move it (callback)
-  const onDrop = async (
-    event: React.DragEvent<HTMLDivElement>,
-  ): Promise<void> => {
+  const onDrop = async (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
     event.stopPropagation();
     setDragCounter(0);
@@ -53,37 +92,36 @@ export const useFileExplorerDragAndDrop = ({
       console.error("Invalid drag data: ", parseResult.error);
       return;
     }
-    const file = parseResult.data!;
-    if (file.path === currentFilePath) {
-      const saveResult = await tryCatch(saveFile(file.path, editorNoteText));
+
+    const draggedFile = parseResult.data!;
+    const currentFilePath = store.get(currentFilePathAtom);
+    const editorNoteText = store.get(editorNoteTextAtom);
+
+    if (draggedFile.path === currentFilePath) {
+      const saveResult = await tryCatch(
+        saveFile(draggedFile.path, editorNoteText)
+      );
       if (saveResult.error) {
         console.error("Failed to save file: ", saveResult.error);
         return;
       }
-      console.log("Moving from", file.path, "to", targetDirectoryPath);
-      const moveResult = await tryCatch(
-        moveFile(file.path, targetDirectoryPath),
-      );
-      if (moveResult.error) {
-        console.error("Failed to move file: ", moveResult.error);
-        return;
-      }
-
-      if (moveResult.data.output) {
-        file.path = moveResult.data.output;
-        open(file, true);
-      }
-    } else {
-      const moveResult = await tryCatch(
-        moveFile(file.path, targetDirectoryPath),
-      );
-      if (moveResult.error) {
-        console.error("Failed to move file:", moveResult.error);
-        return;
-      }
     }
 
-    setReloadFlag((prev) => !prev);
+    const moveResult = await tryCatch(
+      moveFile(draggedFile.path, targetDirectoryPath)
+    );
+
+    if (moveResult.error) {
+      console.error("Failed to move file:", moveResult.error);
+      return;
+    }
+
+    if (draggedFile.path === currentFilePath && moveResult.data.output) {
+      draggedFile.path = moveResult.data.output;
+      open(draggedFile, true);
+    }
+
+    setReloadFlag(!store.get(reloadFlagAtom));
   };
 
   return {
