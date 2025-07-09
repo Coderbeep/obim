5ce9ff88eb0e5ec108d345adac9280056c8ac88b
coderbeep <jakubkubiak234@gmail.com>
2025-07-09 23:04:51 +0200

    fix: doubled state update of currently opened file

--- src/renderer/src/hooks/file-actions-hooks/useNavigation.tsx
+++ src/renderer/src/hooks/file-actions-hooks/useNavigation.tsx
@@ -1,5 +1,4 @@
 import { fileHistoryBackwardStackAtom, fileHistoryForwardStackAtom } from "@renderer/store/FileNavigationStore"
-import { currentFilePathAtom } from "@renderer/store/NotesStore"
 import { useStore } from "jotai"
 import { useFileOpen } from "./useFileActions"
 import { useCallback } from "react"
@@ -16,22 +15,19 @@ export const useNavigation = () => {
     
         const previousFile = backStack[backStack.length - 1];
       
-        store.set(currentFilePathAtom, previousFile.path);
         store.set(fileHistoryForwardStackAtom, [...forwardStack, previousFile]);
         store.set(fileHistoryBackwardStackAtom, backStack.slice(0, -1));
-      
+
         open(backStack[backStack.length - 2], false, true);
       }, [store, open]);
 
     const goForward = useCallback(() => {
         const backStack = store.get(fileHistoryBackwardStackAtom);
         const forwardStack = store.get(fileHistoryForwardStackAtom);
-
         if (forwardStack.length === 0) return;
 
         const nextFile = forwardStack[forwardStack.length - 1];
 
-        store.set(currentFilePathAtom, nextFile.path);
         store.set(fileHistoryBackwardStackAtom, [...backStack, nextFile]);
         store.set(fileHistoryForwardStackAtom, forwardStack.slice(0, -1));
         open(nextFile, false, true);
